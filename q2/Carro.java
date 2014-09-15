3dcc01b8cc53cc0ce0c56fef4e3770f52971aa77
Rafael Ignácio <rafaelignacio@MacBook-Pro-de-Rafael.local>
2014-09-14 21:20:04 -0300

    Atividade 1

---
+++ q2/Carro.java
@@ -0,0 +1,35 @@
+package q2;
+
+
+/**
+ * Escreva a descrição da classe Carro aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class Carro
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private int x;
+
+    /**
+     * COnstrutor para objetos da classe Carro
+     */
+    public Carro()
+    {
+        // inicializa variáveis de instância
+        x = 0;
+    }
+
+    /**
+     * Exemplo de método - substitua este comentário pelo seu próprio
+     * 
+     * @param  y   exemplo de um parâmetro de método
+     * @return     a soma de x com y 
+     */
+    public int sampleMethod(int y)
+    {
+        // ponha seu código aqui
+        return x + y;
+    }
+}
---
+++ q5/ZonaEleitoral.java
@@ -0,0 +1,35 @@
+package q5;
+
+
+/**
+ * Escreva a descrição da classe ZonaEleitoral aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class ZonaEleitoral
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private int x;
+
+    /**
+     * COnstrutor para objetos da classe ZonaEleitoral
+     */
+    public ZonaEleitoral()
+    {
+        // inicializa variáveis de instância
+        x = 0;
+    }
+
+    /**
+     * Exemplo de método - substitua este comentário pelo seu próprio
+     * 
+     * @param  y   exemplo de um parâmetro de método
+     * @return     a soma de x com y 
+     */
+    public int sampleMethod(int y)
+    {
+        // ponha seu código aqui
+        return x + y;
+    }
+}
